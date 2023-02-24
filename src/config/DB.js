6e4c5c86e9b1df57c328b747ef5e9da2872e3e19
Sugey127 <syra478xd@gmail.com>
2023-02-24 13:59:14 -0600

    1:59 pm 24/02/2023

--- src/config/DB.js
+++ src/config/DB.js
@@ -5,7 +5,12 @@ dotenv.config({ path: './.env' });
 
 console.log(process.env.DB_PASS);
 
-export const sequelize = new Sequelize('digilist', 'postgres', process.env.DB_PASS, {
-    host: 'localhost',
-    dialect: 'postgres'
-});  
+/**Base de datos local */
+
+// export const sequelize = new Sequelize('digilist', 'postgres', process.env.DB_PASS, {
+//     host: 'localhost',
+//     dialect: 'postgres'
+// });  
+
+/**Base de datos en la nube */
+export const sequelize = new Sequelize(process.env.DB_URI);
