a3540da31c843fcb6c76cd5b1b15e81bc5612e2b
syra478xd@gmail.com <zeen.cr@gmail.com>
2023-04-22 18:18:35 -0600

    paypay final implementation

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,25 +1,50 @@
 import axios from 'axios';
 import { PAYPLAY_API, PAYPLAY_API_CLIENTE, PAYPLAY_APY_SECRET } from '../config/payment.js';
+import { sequelize } from '../config/DB.js';
+import { Autopartes } from '../models/autopartes.js';
+import { Venta } from '../models/venta.js';
+import { Op } from 'sequelize';
+import { DetalleVenta } from '../models/detalleVenta.js';
+import { escapeXML } from 'ejs';
 
 export const createOrden = async (req, res) => {
     try {
+
+        const { idVenta } = req.query;
+        let totalCarrito = 0;
+        let descriptionCarrito = 'Productos: '
+
+        const carrito = await Venta.findAll({
+            where: { [Op.and]: [{ StatusId: 1 }, { idVenta }] },
+            include: {
+                model: Autopartes
+            }
+        });
+
+        carrito[0].Autopartes.forEach((producto, index) => {
+            totalCarrito += Number(producto.DetalleVenta.precioTotal);
+            descriptionCarrito += index === 0 ? `${producto.nombreAutoparte}: x${producto.DetalleVenta.cantidadVenta} precio: ${producto.DetalleVenta.precioTotal}` : `, ${producto.nombreAutoparte}: x${producto.DetalleVenta.cantidadVenta} precio: ${producto.DetalleVenta.precioTotal}`
+        });
+
+        console.log('total carrito:' + totalCarrito, 'description: ' + descriptionCarrito)
+
         const orden = {
             intent: "CAPTURE",
             purchase_units: [
                 {
                     amount: {
                         currency_code: "MXN",
-                        value: '300.50'
+                        value: totalCarrito
                     },
-                    description: "LLanta buena"
+                    description: descriptionCarrito
                 },
             ],
             application_context: {
                 brand_name: "Digilist.refaccionaria@gmail.com",
                 landing_page: "LOGIN",
                 user_action: "PAY_NOW",
-                return_url: "http://localhost:4000/pagos/capturarOrden",
-                cancel_url: "http://localhost:4000/pagos/cancelarOrden"
+                return_url: `http://localhost:4000/pagos/capturarOrden?idVenta=${idVenta}&UsuarioEmail=${carrito[0].UsuarioEmail}`,
+                cancel_url: `http://localhost:4000/pagos/cancelarOrden`
             }
         };
 
@@ -71,27 +96,53 @@ export const createOrden = async (req, res) => {
 }
 
 export const captureOrden = async (req, res) => {
-    const { token } = req.query;
+    const { token, idVenta } = req.query;
 
     try {
-      const response = await axios.post(
-        `${PAYPLAY_API}/v2/checkout/orders/${token}/capture`,
-        {},
-        {
-          auth: {
-            username: PAYPLAY_API_CLIENTE,
-            password: PAYPLAY_APY_SECRET,
-          },
-        }
-      );
-  
-      console.log(response.data);
-  
-     res.redirect("/payed.html");
+        const response = await axios.post(
+            `${PAYPLAY_API}/v2/checkout/orders/${token}/capture`,
+            {},
+            {
+                auth: {
+                    username: PAYPLAY_API_CLIENTE,
+                    password: PAYPLAY_APY_SECRET,
+                },
+            }
+        );
+
+        const nuevoDetalleVenta = await Venta.findAll({
+            where: { [Op.and]: [{ StatusId: 1 }, { idVenta }] },
+            include: {
+                model: Autopartes
+            }
+        });
+
+        console.log('esta muriendo aqui?')
+
+        // //por cada elemento que itere se actulizará el stock de la autoparte
+        nuevoDetalleVenta[0].Autopartes.forEach(producto => {
+            Autopartes.update({ stockInventario: producto.stockInventario - producto.DetalleVenta.cantidadVenta }, { where: { code_autoparte: producto.code_autoparte } });
+        });
+
+        // console.log(response.data)
+
+        console.log('o quizas aqui?')
+        sequelize.transaction(async t => {
+            await Venta.update({ StatusId: 2 }, { where: { idVenta }, transaction: t });
+            await Venta.create({ UsuarioEmail: req.query.UsuarioEmail, StatusId: 1 }, { transaction: t });
+        })
+
+        console.log(response);
+
+        const userPaypalAccount = await response.data;
+
+        const user = userPaypalAccount.payment_source.paypal.name;
+
+        res.status(200).render('paypal/pagado', { user });
 
     } catch (error) {
-      console.log(error.message);
-      return res.status(500).json({ message: "Internal Server error" });
+        console.log(error.message);
+        return res.status(500).json({ message: "Internal Server error" });
     }
 }
 
--- src/models/venta.js
+++ src/models/venta.js
@@ -1,18 +1,16 @@
 import { sequelize } from "../config/DB.js";
-import { DataTypes } from "sequelize";
+import { DataTypes } from 'sequelize'
 import { Usuario } from "./usuario.js";
 
 export const Venta = sequelize.define('Venta', {
     idVenta: {
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV1,
-
         primaryKey: true,
         allowNull: false
     },
-
-    fechaVenta:{
-        type:DataTypes.DATEONLY,
+    fechaVenta: {
+        type: DataTypes.DATEONLY,
         allowNull: false,
         defaultValue: DataTypes.NOW,
     },
@@ -21,13 +19,13 @@ export const Venta = sequelize.define('Venta', {
 });
 
 Usuario.hasMany(Venta, {
-    foreignKey:{
-        allowNull:false
+    foreignKey: {
+        allowNull: false
     }
 })
 
 Venta.belongsTo(Usuario, {
-    foreignKey:{
-        allowNull:false
+    foreignKey: {
+        allowNull: false
     }
 });
